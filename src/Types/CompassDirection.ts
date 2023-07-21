eddfb4363e1067b43a4cc96ab5cf8c11126ea277
leentaylor <leentaylor@gmail.com>
2023-07-22 07:46:38 +1000

    compass direction implementation and tests

--- src/Types/CompassDirection.ts
+++ src/Types/CompassDirection.ts
@@ -1,11 +1,19 @@
+import { COULD_NOT_PARSE_COMPASS_DIRECTION_SUFFIX } from "./ErrorMessages";
+
 export enum CompassDirection {
-    NORTH = 'NORTH',
-    SOUTH = 'SOUTH',
-    EAST = 'EAST',
-    WEST = 'WEST',
+    NORTH = "NORTH",
+    SOUTH = "SOUTH",
+    EAST = "EAST",
+    WEST = "WEST",
 }
 
 export const directionIsNorthOrSouth = (direction: CompassDirection) =>
     direction === CompassDirection.NORTH || direction === CompassDirection.SOUTH;
 export const directionIsNorthOrEast = (direction: CompassDirection) =>
     direction === CompassDirection.NORTH || direction === CompassDirection.EAST;
+
+export const parseCompassDirection = (direction: string): CompassDirection => {
+    const keys = Object.keys(CompassDirection);
+    if (keys.includes(direction)) return direction as CompassDirection;
+    throw new Error(`${direction} ${COULD_NOT_PARSE_COMPASS_DIRECTION_SUFFIX}`);
+};
---
+++ src/Types/ErrorMessages.ts
@@ -0,0 +1,3 @@
+export const COULD_NOT_PARSE_COORDINATES_SUFFIX = "could not be parsed as coordinates. Values should be numeric.";
+export const COULD_NOT_PARSE_COMPASS_DIRECTION_SUFFIX =
+    "could not be parsed as compass direction. Value should be NORTH, SOUTH, EAST or WEST.";
