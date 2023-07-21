8e4f0cecf72e05e346f6a7ba7b0d183b1a94a4d6
leentaylor <leentaylor@gmail.com>
2023-07-22 08:05:34 +1000

    Coordinates parsing implementation and tests

---
+++ src/Types/Coordinates.test.ts
@@ -0,0 +1,45 @@
+import { parseCoordinates } from "./Coordinates";
+
+describe("Coordinates Parsing", () => {
+    const validCases = [
+        { input1: "2", input2: "4", coordinates: { x: 2, y: 4 } },
+        { input1: "3.5", input2: "5.6", coordinates: { x: 3, y: 5 } },
+    ];
+
+    test.each(validCases)(
+        "Should parse inputs '$input1' and '$input2' as coordinates $coordinates",
+        ({ input1, input2, coordinates }) => {
+            expect(parseCoordinates(input1, input2)).toEqual(coordinates);
+        }
+    );
+
+    const nonNumericInputCases = [
+        { input1: "foo", input2: "4" },
+        { input1: "3.5", input2: "bar" },
+    ];
+
+    test.each(nonNumericInputCases)(
+        "Should throw specific error messages on non numeric input (inputs '$input1' and '$input2')",
+        ({ input1, input2 }) => {
+            const parser = () => parseCoordinates(input1, input2);
+            expect(parser).toThrow(
+                `${input1}, ${input2} could not be parsed as coordinates. Values should be numeric.`
+            );
+        }
+    );
+
+    const belowZeroInputCases = [
+        { input1: "-2", input2: "4" },
+        { input1: "3.5", input2: "-3" },
+    ];
+
+    test.each(belowZeroInputCases)(
+        "Should throw specific error messages on below zero input (inputs '$input1' and '$input2')",
+        ({ input1, input2 }) => {
+            const parser = () => parseCoordinates(input1, input2);
+            expect(parser).toThrow(
+                `${input1}, ${input2} could not be parsed as coordinates. Values should be zero or greater.`
+            );
+        }
+    );
+});
--- src/Types/Coordinates.ts
+++ src/Types/Coordinates.ts
@@ -1,4 +1,17 @@
+import {
+    COULD_NOT_PARSE_COORDINATES_BELOW_ZERO_SUFFIX,
+    COULD_NOT_PARSE_COORDINATES_NON_NUMERIC_SUFFIX,
+} from "./ErrorMessages";
+
 export type Coordinates = {
     x: number;
     y: number;
+};
+
+export const parseCoordinates = (X: string, Y: string): Coordinates => {
+    const x = parseInt(X, 10);
+    const y = parseInt(Y, 10);
+    if (isNaN(x) || isNaN(y)) throw new Error(`${X}, ${Y} ${COULD_NOT_PARSE_COORDINATES_NON_NUMERIC_SUFFIX}`);
+    if (x < 0 || y < 0) throw new Error(`${X}, ${Y} ${COULD_NOT_PARSE_COORDINATES_BELOW_ZERO_SUFFIX}`);
+    return { x, y };
 };
