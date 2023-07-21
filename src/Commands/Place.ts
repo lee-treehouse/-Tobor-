bc484cda6eb012fc108900d219ab33290ca47078
leentaylor <leentaylor@gmail.com>
2023-07-22 08:44:24 +1000

    place changes

--- src/Commands/Place.ts
+++ src/Commands/Place.ts
@@ -1,16 +1,24 @@
-import { Position } from '../Types/Position';
-import { ICommand } from './ICommand';
+import { parseCompassDirection } from "../Types/CompassDirection";
+import { parseCoordinates } from "../Types/Coordinates";
+import { Position } from "../Types/Position";
+import { COULD_NOT_PARSE_PLACE_ARGUMENTS_EXPECTED_3_SUFFIX } from "./ErrorMessages";
+import { ICommand } from "./Command";
 
 export class PlaceCommand implements ICommand {
-    public static command = 'PLACE';
+    public static command = "PLACE";
 
     public readonly canBeIgnored = false;
 
-    public constructor(args?: string[]) {}
+    private position: Position;
+
+    public constructor(args?: string[]) {
+        if (!args || args.length != 3) throw new Error(COULD_NOT_PARSE_PLACE_ARGUMENTS_EXPECTED_3_SUFFIX);
+        const coordinates = parseCoordinates(args[0], args[1]);
+        const directionFacing = parseCompassDirection(args[2]);
+        this.position = { coordinates, directionFacing };
+    }
 
     public execute(currentPosition: Position): Position | void {
-        console.log(
-            `I am ${PlaceCommand.command} command and my current direction is ${currentPosition.directionFacing}`
-        );
+        return this.position;
     }
 }
