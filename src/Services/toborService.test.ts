f6ea7384d846cba3906da764a5c010060856b1dd
leentaylor <leentaylor@gmail.com>
2023-07-22 16:46:21 +1000

    tobor service tests

---
+++ src/Services/toborService.test.ts
@@ -0,0 +1,63 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+import { CompassDirection } from "../Common/CompassDirection";
+import { Position } from "../Common/Position";
+import { Table } from "../Common/Table";
+import { getDefaultConfig } from "../Config/Config";
+import { ToborService } from "./toborService";
+
+const config = getDefaultConfig();
+const table = new Table(config.table);
+let service: ToborService;
+
+beforeEach(() => {
+    service = new ToborService(config.tobor, table);
+});
+
+describe("onReadInput", () => {
+    it("Should not have position change or console.log from commands that can be ignored when robot is not on table", async () => {
+        const consoleSpy = jest.spyOn(console, "log");
+
+        await service.onReadInput("MOVE");
+        expect(service.robotPosition).toBe("OFF");
+
+        await service.onReadInput("LEFT");
+        expect(service.robotPosition).toBe("OFF");
+
+        await service.onReadInput("RIGHT");
+        expect(service.robotPosition).toBe("OFF");
+
+        await service.onReadInput("REPORT");
+        expect(service.robotPosition).toBe("OFF");
+
+        expect(consoleSpy).not.toHaveBeenCalled();
+    });
+
+    it("Should have position change or console.log from commands that can be ignored when robot is on table", async () => {
+        const consoleSpy = jest.spyOn(console, "log");
+
+        await service.onReadInput("PLACE 0,0,NORTH");
+
+        await service.onReadInput("MOVE");
+        expect(service.robotPosition).toEqual({ coordinates: { x: 0, y: 1 }, directionFacing: CompassDirection.NORTH });
+        await service.onReadInput("RIGHT");
+        expect(service.robotPosition).toEqual({ coordinates: { x: 0, y: 1 }, directionFacing: CompassDirection.EAST });
+        await service.onReadInput("LEFT");
+        expect(service.robotPosition).toEqual({ coordinates: { x: 0, y: 1 }, directionFacing: CompassDirection.NORTH });
+
+        await service.onReadInput("REPORT");
+
+        expect(consoleSpy).toHaveBeenCalledWith(`0,1,NORTH`);
+    });
+
+    it("Should have position change from command that can't be ignored when robot is not on table", async () => {
+        await service.onReadInput("PLACE 3,4,SOUTH");
+        const expected: Position = { coordinates: { x: 3, y: 4 }, directionFacing: CompassDirection.SOUTH };
+        expect(service.robotPosition).toEqual(expected);
+    });
+
+    it("Shouldn't have position changed if position is out of bounds of table", async () => {
+        await service.onReadInput("PLACE 3,5,SOUTH");
+        expect(service.robotPosition).toBe("OFF");
+    });
+});
--- src/Services/toborService.ts
+++ src/Services/toborService.ts
@@ -8,7 +8,7 @@ import { cliInputService } from "./cliInputService";
 import { FileReadingService } from "./fileReadingService";
 
 export class ToborService {
-    private robotPosition: Position | "OFF" = "OFF";
+    public robotPosition: Position | "OFF" = "OFF";
 
     public constructor(private readonly config: ToborConfig, private readonly table: Table) {}
 
@@ -16,16 +16,17 @@ export class ToborService {
         const commandInput = separateCommandAndArguments(line, this.config.input.format.capitaliseCommandsAndArgs);
         const command: ICommand = getCommand(commandInput);
 
-        if (this.robotPosition === "OFF" && command.canBeIgnored) Promise.resolve();
+        if (this.robotPosition === "OFF" && command.canBeIgnored) return Promise.resolve();
 
         const newPosition = command.execute(this.robotPosition !== "OFF" ? this.robotPosition : defaultPosition);
+
         if (newPosition && !this.table.areCoordinatesOutOfBounds(newPosition.coordinates))
             this.robotPosition = newPosition;
 
         Promise.resolve();
     };
 
-    readInput = async () => {
+    public readInput = async () => {
         if (this.config.input.fileName) {
             const fileReadingService = new FileReadingService(this.config.input.fileName);
             await fileReadingService.processFileLineByLine(this.onReadInput);
