2cfc1184d292ce1886fc8883f421163a40f5a03a
leentaylor <leentaylor@gmail.com>
2023-07-22 16:46:30 +1000

    rename method

--- src/Services/cliInputService.ts
+++ src/Services/cliInputService.ts
@@ -2,7 +2,7 @@ import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
 export class cliInputService {
-    public async requestInputLineByLine(cb: (line: string) => Promise<void>) {
+    public async requestInputLineByLine(onReadLine: (line: string) => Promise<void>) {
         const rl = readline.createInterface({ input, output });
 
         console.log("Here is some intro and helper text");
@@ -11,7 +11,7 @@ export class cliInputService {
         while (userInput !== "EXIT") {
             userInput = await rl.question("Enter command or type EXIT to exit. \n");
             // TODO why is rl.question console.logging the answer?
-            await cb(userInput);
+            await onReadLine(userInput);
         }
     }
 }
--- src/Services/fileReadingService.ts
+++ src/Services/fileReadingService.ts
@@ -2,11 +2,11 @@ import { open } from "node:fs/promises";
 export class FileReadingService {
     public constructor(private readonly filename: string) {}
 
-    public async processFileLineByLine(cb: (line: string) => Promise<void>) {
+    public async processFileLineByLine(onReadLine: (line: string) => Promise<void>) {
         const file = await open(this.filename);
 
         for await (const line of file.readLines()) {
-            await cb(line);
+            await onReadLine(line);
         }
     }
 }
