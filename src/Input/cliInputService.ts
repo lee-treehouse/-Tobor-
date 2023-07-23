bb2de399bbdf7b7ca2314ec047f588e14a51dc7d
leentaylor <leentaylor@gmail.com>
2023-07-24 08:31:36 +1000

    verified fix for echoing user input bug I had, but this fix gives me an idea...

--- src/Input/cliInputService.ts
+++ src/Input/cliInputService.ts
@@ -1,5 +1,4 @@
 import * as readline from "node:readline/promises";
-import { stdin as input, stdout as output } from "node:process";
 import { TOBOR_COMMAND_PROMPT, TOBOR_HELP_TEXT, TOBOR_WELCOME } from "../UX/messages";
 import { LineReader } from "./LineReader";
 import { Logger } from "../Output/Logger";
@@ -9,16 +8,28 @@ const LOOP_UNTIL_EXIT_COMMAND = true;
 export class cliInputService implements LineReader {
   public constructor(private readonly logger: Logger) {}
 
-  public async getInputLineByLine(onReadLine: (line: string) => Promise<void>) {
-    const rl = readline.createInterface({ input, output });
+  private readLineAsync = async (): Promise<string> => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+    });
 
+    return new Promise((resolve) => {
+      rl.prompt();
+      rl.on("line", (line: string) => {
+        rl.close();
+        resolve(line);
+      });
+    });
+  };
+
+  public async getInputLineByLine(onReadLine: (line: string) => Promise<void>) {
     this.logger.log(TOBOR_WELCOME);
     this.logger.log(TOBOR_HELP_TEXT);
 
     while (LOOP_UNTIL_EXIT_COMMAND) {
-      const userInput = await rl.question(`${TOBOR_COMMAND_PROMPT}\n`);
+      this.logger.log(`${TOBOR_COMMAND_PROMPT}`);
+      const userInput = await this.readLineAsync();
 
-      // TODO why is rl.question console.logging the answer?
       await onReadLine(userInput);
     }
   }
