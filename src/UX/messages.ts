503c21d3eb719e4043b9dabbc6e6d9f5cfdd5a38
leentaylor <leentaylor@gmail.com>
2023-07-23 13:04:33 +1000

    add helper text that commands will be ignored until robot is placed

--- src/UX/messages.ts
+++ src/UX/messages.ts
@@ -1,7 +1,7 @@
 export const TOBOR_WELCOME = "Hi, I'm Tobor. I can help you move an imaginary robot around an imaginary table.\n";
 
 export const TOBOR_HELP_TEXT =
-    "Permitted commands are LEFT, MOVE, PLACE, REPORT, and RIGHT.\nPLACE command takes three arguments (X coordinate, Y coordinate, Direction) eg 'PLACE 1,2,NORTH'\n";
+    "Permitted commands are LEFT, MOVE, PLACE, REPORT, and RIGHT. \nPLACE command takes three arguments (X coordinate, Y coordinate, Direction) eg 'PLACE 1,2,NORTH' \nAll commands except PLACE are ignored until the robot is placed on the table.\n";
 
 export const TOBOR_COMMAND_PROMPT = "Please enter a command or type 'EXIT' to exit the application.";
 
