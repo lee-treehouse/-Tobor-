b906ab1519166a504a55430444caeb7d1cfdfe6f
leentaylor <leentaylor@gmail.com>
2023-07-23 13:34:43 +1000

    add error handling to file reading service

--- src/Services/fileReadingService.ts
+++ src/Services/fileReadingService.ts
@@ -1,12 +1,27 @@
 import { open } from "node:fs/promises";
+import { TOBOR_FILE_HANDLING_ERROR_PREFIX } from "../ErrorMessages/FileHandling";
 export class FileReadingService {
     public constructor(private readonly filename: string) {}
 
-    public async processFileLineByLine(onReadLine: (line: string) => Promise<void>) {
-        const file = await open(this.filename);
+    handleError = (error: unknown) => {
+        let message = "Unknown Error";
+        if (error instanceof Error) message = error.message;
+        console.log(TOBOR_FILE_HANDLING_ERROR_PREFIX);
+        console.log(message);
+    };
 
-        for await (const line of file.readLines()) {
-            await onReadLine(line);
+    public async processFileLineByLine(onReadLine: (line: string) => Promise<void>) {
+        let file;
+
+        try {
+            file = await open(this.filename);
+            for await (const line of file.readLines()) {
+                await onReadLine(line);
+            }
+        } catch (error) {
+            this.handleError(error);
+        } finally {
+            await file?.close();
         }
     }
 }
