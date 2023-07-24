1f76354df60ef31aee357e1669a3dec2736845ff
leentaylor <leentaylor@gmail.com>
2023-07-24 17:46:02 +1000

    fix error type for coordinates

--- src/Common/Coordinates.ts
+++ src/Common/Coordinates.ts
@@ -1,3 +1,4 @@
+import { CommandParserError } from "../Commands/CommandParserError";
 import {
   COULD_NOT_PARSE_COORDINATES_BELOW_ZERO_SUFFIX,
   COULD_NOT_PARSE_COORDINATES_NON_NUMERIC_SUFFIX,
@@ -12,7 +13,8 @@ export type Coordinates = {
 export const parseCoordinates = (X: string, Y: string): Coordinates => {
   const x = parseInt(X, 10);
   const y = parseInt(Y, 10);
-  if (isNaN(x) || isNaN(y)) throw new Error(`${X}, ${Y} ${COULD_NOT_PARSE_COORDINATES_NON_NUMERIC_SUFFIX}`);
-  if (x < 0 || y < 0) throw new Error(`${X}, ${Y} ${COULD_NOT_PARSE_COORDINATES_BELOW_ZERO_SUFFIX}`);
+  if (isNaN(x) || isNaN(y))
+    throw new CommandParserError(`${X}, ${Y} ${COULD_NOT_PARSE_COORDINATES_NON_NUMERIC_SUFFIX}`);
+  if (x < 0 || y < 0) throw new CommandParserError(`${X}, ${Y} ${COULD_NOT_PARSE_COORDINATES_BELOW_ZERO_SUFFIX}`);
   return { x, y };
 };
