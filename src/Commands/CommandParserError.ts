a9423477802b36e701de8ed95db3bb4f563e8ae9
leentaylor <leentaylor@gmail.com>
2023-07-24 13:55:05 +1000

    add todo

--- src/Commands/CommandParserError.ts
+++ src/Commands/CommandParserError.ts
@@ -1,3 +1,4 @@
+// TODO add checks to tests that throw this error to confirm they are throwing it and not just error
 export class CommandParserError extends Error {
   constructor(message: string) {
     super(message);
