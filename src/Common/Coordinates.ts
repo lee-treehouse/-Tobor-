8eb8a6ffc7bb846e283809a0682baeffdef58286
leentaylor <leentaylor@gmail.com>
2023-08-07 16:36:00 +1000

    experimental

--- src/Common/Coordinates.ts
+++ src/Common/Coordinates.ts
@@ -4,12 +4,38 @@ import {
   COULD_NOT_PARSE_COORDINATES_NON_NUMERIC_SUFFIX,
 } from "../ErrorMessages/Parsing";
 
+export type Coordinate = number | LogicalCoordinate;
+
 export type Coordinates = {
-  x: number;
-  y: number;
+  x: Coordinate;
+  y: Coordinate;
 };
 
-export const parseCoordinates = (X: string, Y: string): Coordinates => {
+export enum LogicalCoordinate {
+  MAX = "MAX",
+  RANDOM = "RANDOM",
+}
+
+export const isLogicalCoordinate = (coordinate: Coordinate): coordinate is LogicalCoordinate => {
+  if (typeof coordinate === "number") return false;
+  return true;
+};
+
+const canBeParsedAsLogicalCoordinate = (coordinate: string): coordinate is LogicalCoordinate => {
+  if (typeof coordinate === "number") return false;
+  const validLogicalCoordinates = Object.keys(LogicalCoordinate);
+  return validLogicalCoordinates.includes(coordinate);
+};
+
+export const parseLogicalCoordinate = (coordinate: string): LogicalCoordinate => {
+  if (canBeParsedAsLogicalCoordinate(coordinate)) return coordinate;
+  // do something about this
+  throw new CommandParserError(`${coordinate} could not be parsed as a logical coordinate`);
+};
+
+export const parseCoordinates = (X: string, Y: string, allowLogicalCoordinates = false): Coordinates => {
+  if (allowLogicalCoordinates) throw new Error("Not yet implemented");
+
   const x = parseInt(X, 10);
   const y = parseInt(Y, 10);
   if (isNaN(x) || isNaN(y))
--- src/Common/Table.ts
+++ src/Common/Table.ts
@@ -1,13 +1,35 @@
 import { TableConfig } from "../Config/Config";
-import { Coordinates } from "./Coordinates";
+import { Coordinates, LogicalCoordinate, isLogicalCoordinate } from "./Coordinates";
 
 export class Table {
   public constructor(private readonly config: TableConfig) {}
 
+  interpretCoordinates = (coordinates: Coordinates) => {
+    const { x, y } = coordinates;
+
+    const interpretX = isLogicalCoordinate(x);
+    const interpretY = isLogicalCoordinate(y);
+
+    const interpretedX = interpretX ? this.interpretLogicalCoordinate(x, this.config.size.width) : x;
+    const interpretedY = interpretY ? this.interpretLogicalCoordinate(y, this.config.size.length) : y;
+
+    return { x: interpretedX, y: interpretedY };
+  };
+
+  interpretLogicalCoordinate(coordinate: LogicalCoordinate, max: number) {
+    if (coordinate === LogicalCoordinate.RANDOM) throw new Error("Not yet implemented");
+    return max;
+  }
+
   areCoordinatesOutOfBounds = (coordinates: Coordinates) => {
-    if (coordinates.x < 0 || coordinates.y < 0) return true;
-    if (coordinates.x > this.config.size.width - 1) return true;
-    if (coordinates.y > this.config.size.length - 1) return true;
+    const { x, y } = coordinates;
+
+    const evaluateX = !isLogicalCoordinate(x);
+    const evaluateY = !isLogicalCoordinate(y);
+
+    if (evaluateX && (x < 0 || x > this.config.size.width - 1)) return true;
+    if (evaluateY && (y < 0 || y > this.config.size.length - 1)) return true;
+
     return false;
   };
 }
