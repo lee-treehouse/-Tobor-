f610fec2fda9e4a5328046de66465b075a642365
leentaylor <leentaylor@gmail.com>
2023-08-16 13:56:28 +1000

    create algo test config

--- src/__TESTS__/TestFiles/Config/DefaultTestConfig.ts
+++ src/__TESTS__/TestFiles/Config/DefaultTestConfig.ts
@@ -21,3 +21,34 @@ export const getDefaultTestConfig = (): AppConfig => {
     },
   };
 };
+
+export const getAlgoTestConfig = (): AppConfig => {
+  return {
+    tobor: {
+      input: {
+        parser: {
+          exitOnCommandParserError: false,
+        },
+        format: {
+          capitaliseCommandsAndArgs: false,
+        },
+      },
+    },
+    table: {
+      size: {
+        width: 6,
+        length: 5,
+      },
+      obstaclesCoordinates: [
+        { x: 1, y: 4 },
+        { x: 1, y: 3 },
+        { x: 1, y: 2 },
+        { x: 2, y: 2 },
+        { x: 2, y: 1 },
+        { x: 3, y: 4 },
+        { x: 3, y: 3 },
+        { x: 3, y: 2 },
+      ],
+    },
+  };
+};
